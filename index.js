4ae61d347c2d17500cc9305f1a8fcd0351963d3f
agraharidheeraj <dheeraj700agrahari@gmail.com>
2023-04-20 21:00:30 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ http.listen(PORT, () =>{
 })
 app.use(express.static(__dirname + '/public'))
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/project.html')
 })
 
 // Socket
